6bdbee23a4d300015fca3f8469a1010883aedd2f
David LaMartina <lamartid@oregonstate.edu>
2019-04-29 22:14:23 -0700

    Finished Project 3

--- Project3/proj3.cpp
+++ Project3/proj3.cpp
@@ -8,11 +8,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <float.h>
 #include <omp.h>
 
 #include "helpers.h"
 
-#define NUMTHREADS          3
+#define NUMTHREADS          4
 #define FIRST_YEAR          2019
 #define LAST_YEAR           2024
 
@@ -20,11 +21,13 @@
 int     NowYear;            // 2019 - 2024
 int     NowMonth;           // 0 - 11
 
-float   NowAngle;           // Angle used for temp & precip calcs - should be same for given month
+float   NowAngle;           // Angle used for temp & precip calcs
 float   NowPrecip;          // inches of rain per month
 float   NowTemp;            // temperature this month
 float   NowHeight;          // grain height in inches
 int     NowNumDeer;         // number of deer in current population
+int     NowNumBears;        // number of bears in current population
+float   DeerToBears;        // Ratio of deer to bears
 
 // Params defaults
 // Grain growth in inches
@@ -44,11 +47,13 @@ const float RANDOM_TEMP             =       10.0;   // plus or minus noise
 const float MIDTEMP                 =       40.0;
 const float MIDPRECRIP              =       10.0;
 
+const float DB_EQUILIBRIUM          =       2.0;    // Equilibrium b/w deer and bears
+
 // Prototypes
 void GrainDeer(unsigned int*);
 void Grain(unsigned int*);
 void Watcher(unsigned int*);
-void MyAgent();
+void Bears(unsigned int*);
 void UpdateState(unsigned int*);
 int  AbsoluteMonth(int year, int month);
 
@@ -60,10 +65,11 @@ main(int argc, char* argv[]){
 
     unsigned int seedI = time(0);   // Separate seed for main & each thread
     NowNumDeer  =   1;
+    NowNumBears =   1;
     NowHeight   =   1.;
     UpdateState(&seedI);
  
-    printf("Month\tPrecip\tTemp\tHeight\tDeer\n");
+    printf("Month\tPrecip\tTemp\tHeight\tDeer\tBears\n");
 
     omp_set_num_threads(NUMTHREADS);    // same as # decomposed functions
     #pragma omp parallel sections
@@ -83,8 +89,11 @@ main(int argc, char* argv[]){
             unsigned int seedW = time(0);
             Watcher(&seedW);
         }
-        //#pragma omp section
-        //{ MyAgent(); }
+        #pragma omp section
+        {
+            unsigned int seedB = time(0);
+            Bears(&seedB);
+        }
     }
     return 0;
 }
@@ -94,6 +103,7 @@ void GrainDeer(unsigned int* seedp){
     // Increment or decrement based on available grain
     int deltaDeer;
     while(NowYear <= LAST_YEAR){
+        // Account for grain height
         if (NowNumDeer > NowHeight){
             deltaDeer = -1;
         }
@@ -103,9 +113,39 @@ void GrainDeer(unsigned int* seedp){
         else{
             deltaDeer = 0;
         }
+        // Account for bears
+        if (DeerToBears > 2){
+            deltaDeer -= 1;
+        }
         #pragma omp barrier // compute barrier
 
         NowNumDeer += deltaDeer;
+        if (NowNumDeer < 0)
+            NowNumDeer = 0;
+        #pragma omp barrier // assign barrier
+
+        #pragma omp barrier // output & update barrier
+    }
+}
+
+// Bears eat deer! But they'll die off if there aren't enough deer around.
+void Bears(unsigned int* seedp){
+    int deltaBears;
+    while(NowYear <= LAST_YEAR){
+        if (NowNumBears > NowNumDeer){
+            deltaBears = -1;
+        }
+        else if (NowNumBears < NowNumDeer){
+            deltaBears = 1;
+        }
+        else{
+            deltaBears = 0;
+        }
+        #pragma omp barrier // compute barrier
+
+        NowNumBears += deltaBears;
+        if (NowNumBears <= 0)
+            NowNumBears = 0;
         #pragma omp barrier // assign barrier
 
         #pragma omp barrier // output & update barrier
@@ -139,12 +179,13 @@ void Watcher(unsigned int* seedp){
         #pragma omp barrier // assign barrier
 
         // Output all globals
-        printf("%d\t%.3lf\t%.3lf\t%.3lf\t%d\n",
+        printf("%d\t%.3lf\t%.3lf\t%.3lf\t%d\t%d\n",
                 AbsoluteMonth(NowYear, NowMonth),
                 NowPrecip,
                 FtoC(NowTemp),
                 NowHeight,
-                NowNumDeer);
+                NowNumDeer,
+                NowNumBears);
 
         // Update globals
         NowMonth++;
@@ -155,15 +196,19 @@ void Watcher(unsigned int* seedp){
         UpdateState(seedp);
         #pragma omp barrier // output & update barrier
     }
-
 }
 
-void MyAgent(){
-
-    return;
-}
 
 // Update state 
+void UpdateDeerToBears(){
+    if (NowNumBears <= 0){
+        DeerToBears = FLT_MAX;
+    }
+    else{
+        DeerToBears = (float)NowNumDeer / (float)NowNumBears;
+    }
+}
+
 void UpdateState(unsigned int* seedp){
     NowAngle    = Angle(NowMonth);
     NowPrecip   = Precip(AVG_PRECIP_PER_MONTH,
@@ -176,7 +221,9 @@ void UpdateState(unsigned int* seedp){
                          RANDOM_TEMP,
                          NowAngle,
                          seedp);
+    UpdateDeerToBears();
 }
+
 
 // Calculate exact month for graph-friendly output
 int AbsoluteMonth(int year, int month){
