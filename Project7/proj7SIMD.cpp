2153e754e5fcba5f3f867d6936d1fe66c0ac28a3
David LaMartina <lamartid@oregonstate.edu>
2019-05-31 05:26:45 -0700

    Finished SIMD code

--- Project7/proj7SIMD.cpp
+++ Project7/proj7SIMD.cpp
@@ -7,12 +7,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <omp.h>
+
+#include "simd.p4.h"
+
+#ifndef WRITENUMS
+#define WRITENUMS   512
+#endif
 
 int     Size;
 float*  Array;
 float*  Sums;
 FILE*   fp;
-int     i;
 
 void readData( )
 {
@@ -28,7 +36,7 @@ void readData( )
     Sums  = (float*)malloc( 1 * Size * sizeof( float ) );
 
     // Scan in and duplicate the array to produce "wraparound" effect
-    for( i = 0; i < Size; i++ )
+    for( int i = 0; i < Size; i++ )
     {
         fscanf( fp, "%f", &Array[i] );
         Array[ i + Size ] = Array[i];
@@ -36,9 +44,39 @@ void readData( )
     fclose( fp );
 }
 
+void writeData( )
+{
+    fp = fopen( "resultsSIMD.txt", "w" );
+    if( fp == NULL )
+    {
+        fprintf( stderr, "Cannot open file 'resultsSIMD.txt' for writing\n" );
+        exit( 1 );
+    }
+    for( int i = 0; i < WRITENUMS; i++ )
+    {
+        fprintf( fp, "%f\n", Sums[ i ] );
+    }
+    fclose( fp );
+}
+
 int main( int argc, char* argv[] )
 {
+    readData();
 
+    double time0 = omp_get_wtime();
+
+    for( int shift = 0; shift < Size; shift++ )
+    {
+        Sums[ shift ] = SimdMulSum( &Array[ 0 ], &Array[ 0 + shift ], Size );
+    }
+
+    double time1 = omp_get_wtime();
+    double megaFuncPerSecond = (double)Size / ( time1 - time0 ) / 1000000.;
+    printf( "Performance: %f megaFunctions per second\n", megaFuncPerSecond );
+
+    writeData();
+    free( Array );
+    free( Sums  );
 
     return 0;
 }
---
+++ Project7/simd.p4.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctime>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+#include <omp.h>
+
+#ifndef SIMD_H
+#define SIMD_H
+
+// SSE stands for Streaming SIMD Extensions
+
+#define SSE_WIDTH	4
+
+#define ALIGNED		__attribute__((aligned(16)))
+
+// SIMD multiplication & reduction functions
+void	SimdMul(        float *, float *,  float *, int );
+float	SimdMulSum(     float *, float *, int );
+
+// Non-SIMD multiplication & reduction functions
+void    NonSimdMul(     float *, float *,  float *, int );
+float   NonSimdMulSum(  float *, float *, int );
+
+
+#endif		// SIMD_H
